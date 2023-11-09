2a8d34612195fe8c1c1e83230b15948cf097f1e1
Pniaq23 <88191801+Pniaq23@users.noreply.github.com>
2023-11-09 15:40:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,6 +123,18 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "git"
+    priority                   = 1008
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "108"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
